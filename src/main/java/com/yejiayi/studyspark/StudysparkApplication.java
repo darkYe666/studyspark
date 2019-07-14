8ce85d1b368dc683a4026d9001f37435ed7eb6b9
darkYe666 <8938202482qq.com>
2019-07-14 16:03:37 +0800

    Initial commit

---
+++ src/main/java/com/yejiayi/studyspark/StudysparkApplication.java
@@ -0,0 +1,13 @@
+package com.yejiayi.studyspark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudysparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudysparkApplication.class, args);
+    }
+
+}
