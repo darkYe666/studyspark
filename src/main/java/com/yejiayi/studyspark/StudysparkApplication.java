9391bacfca4ff859c80eeec8fadd6b1fa701ec55
darkYe666 <8938202482qq.com>
2019-09-03 09:11:21 +0800

    新增sprakcore

--- src/main/java/com/yejiayi/studyspark/StudysparkApplication.java
+++ src/main/java/com/yejiayi/studyspark/StudysparkApplication.java
@@ -1,13 +1,55 @@
 package com.yejiayi.studyspark;
 
+//import org.apache.spark.SparkConf;
+//import org.apache.spark.api.java.JavaPairRDD;
+//import org.apache.spark.api.java.JavaRDD;
+//import org.apache.spark.api.java.JavaSparkContext;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
+//import scala.Tuple2;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.apache.commons.lang3.StringUtils.SPACE;
+
+//import static org.apache.commons.lang3.StringUtils.SPACE;
 
 @SpringBootApplication
 public class StudysparkApplication {
 
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(StudysparkApplication.class, args);
+
+        SparkConf sparkConf =  new SparkConf().setAppName("wordCount").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+
+        JavaRDD<String> lines = sc.textFile("/Users/ye/Documents/MySelfProjects/studyspark/src/main/java/com/yejiayi/studyspark/users.txt");
+
+        JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
+
+        JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
+
+        JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
+
+        List<Tuple2<String, Integer>> output = counts.collect();
+        for (Tuple2<?,?> tuple : output) {
+            System.out.println(tuple._1() + ": " + tuple._2());
+        }
+
     }
 
 }
